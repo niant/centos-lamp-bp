a08080ac8b4ee5f5069e4bbef87e720d2c0ddbe3
niant <antti.niemenpaa@gmail.com>
2013-03-22 13:44:39 +0200

    Switched to Apache module that supports CentOS.

--- puppet/manifests/init.pp
+++ puppet/manifests/init.pp
@@ -1,3 +1,15 @@
 class { 'yum':
     extrarepo => ['epel', 'remi']
 }
+
+apache_httpd { 'prefork':
+    ssl => true,
+    modules => [
+        'auth_basic',
+        'mime',
+        'dir',
+        'alias',
+        'rewrite'
+    ]
+    # welcome => false,
+}
