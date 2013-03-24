222b36085264e85f5e1384bde37ac8c51a6b6cdc
niant <antti.niemenpaa@gmail.com>
2013-03-24 19:11:22 +0200

    Removed PHP module.

--- puppet/manifests/init.pp
+++ puppet/manifests/init.pp
@@ -1,5 +1,28 @@
+# http://blog.servergrove.com/2013/01/11/creating-development-environments-with-vagrant-and-puppet/comment-page-1/
+#
+# https://sites.google.com/site/timyimblog/blog/deploying-php53-on-centos-with-puppet
+#
+# https://github.com/example42/puppet-yum
+#
+# https://github.com/thias/puppet-php
+#
+# https://github.com/thias/puppet-apache_httpd
+#
 class { 'yum':
     extrarepo => ['epel', 'remi']
+}
+
+class apache {
+    package { 'httpd':
+        name => 'httpd',
+        ensure => installed
+    }
+
+    service {'httpd':
+        ensure => 'httpd',
+        ensure => running,
+        require => Package['httpd']
+    }
 }
 
 apache_httpd { 'prefork':
@@ -13,3 +36,6 @@ apache_httpd { 'prefork':
     ]
     # welcome => false,
 }
+
+# php::module { [ 'pecl-apc', 'xml' ]: }
+
